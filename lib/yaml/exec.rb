32d8ab9048ce344564f58b609cd7dc5bdcbc20ca
TakiuchiGenki <genki@s21g.com>
2015-08-17 01:51:24 +0900

    Added exe and test

--- lib/yaml/exec.rb
+++ lib/yaml/exec.rb
@@ -1,7 +1,46 @@
 require "yaml/exec/version"
+require 'yaml'
 
 module Yaml
   module Exec
-    # Your code goes here...
+    class Cli
+      def initialize
+        *args, path = ARGV
+        abort 'command is required for 1st arg' if args.empty?
+        abort 'yml file is needed for 2nd arg' if path.nil?
+        @cmd = args.join ' '
+        @yml = YAML.load_file path
+      end
+
+      def run
+        cmd_list @yml
+      end
+
+    private
+      def cmd_list(hash)
+        out = @cmd.dup
+        hash.each do |value, args|
+          cmd_args args, out
+          out.concat " #{value}"
+        end
+        puts out
+      end
+
+      def cmd_args(args, out)
+        args.each do |(k,v)|
+          case v
+          when Hash; cmd_arg_hash(k, v, out)
+          when Array
+          else out.concat " --#{k} #{v}"
+          end
+        end
+      end
+
+      def cmd_arg_hash(key, hash, out)
+        hash.each do |k,v|
+          out.concat " --#{key} #{k}=#{v}"
+        end
+      end
+    end
   end
 end
